dd299a5b63247e23dad6df3d7b5e0c41b20f3c8a
David <dshardman@outlook.com>
2022-02-17 10:39:07 +0000

    videos so far

--- animateResponse.m
+++ animateResponse.m
@@ -23,3 +23,23 @@ for k = 0:length(q)/1000
 %         drawnow
 %     end
 end
+
+%%
+q = squareprobe(20:end-20,2);
+
+v = VideoWriter('matlabvideo');
+v.FrameRate = 50;
+v.Quality = 100;
+open(v);
+for i = 1:2000:length(q)
+    plot(0:0.02:(i-1)/100000, q(1:2000:i), 'Color', 'b', 'LineWidth', 2);
+    axis([0,40,0.62,0.76]);
+    set(gca, 'LineWidth', 2, 'FontSize', 15);
+    set(gcf, 'Color', 'w');
+    xlabel('Time (s)');
+    ylabel('Voltage (V)');
+    box off
+    set(gcf, 'Position', 1000*[0.3394    0.4380    1.0432    0.4200]);
+    v.writeVideo(getframe(gcf));
+end
+close(v);
